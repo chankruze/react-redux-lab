35636c76a1457ff13671dcad8c20c8451f66bcda
chankruze <chankruze@gmail.com>
2020-09-15 16:44:38 +0530

    Adds Logger Middleware to React App
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,10 +5,11 @@ Created: Tue Sep 15 2020 09:41:02 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2020 and beyond
 */
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
+import logger from 'redux-logger'
 
 // create redux
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 export default store
