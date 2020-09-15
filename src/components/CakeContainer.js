50f5ea8b542bdd0c6b61b98c69f75b0713ca73fa
chankruze <chankruze@gmail.com>
2020-09-15 09:47:15 +0530

    Creates a react component for cake container
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,19 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Sep 15 2020 09:31:01 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy A Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
