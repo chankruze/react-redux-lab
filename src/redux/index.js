9228e7eed76529947aaa27a13987761cd94a910e
chankruze <chankruze@gmail.com>
2020-09-15 10:01:24 +0530

    Adds a dedicated file to export actions
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/redux/index.js
@@ -0,0 +1,8 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Sep 15 2020 09:54:01 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+export { buyCake } from './cake/cakeActions'
