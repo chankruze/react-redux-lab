3a4c262375b2134ecff33d0c9bdb486a235078a5
chankruze <chankruze@gmail.com>
2020-09-15 09:45:39 +0530

    Creates Action Type
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,14 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Sep 15 2020 09:34:31 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import { BUY_CAKE } from "./cakeTypes"
+
+export const buyCake = () => {
+    return {
+        type: BUY_CAKE
+    }
+}
---
+++ src/redux/cake/cakeTypes.js
@@ -0,0 +1,8 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Sep 15 2020 09:35:55 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+export const BUY_CAKE = 'BUY_CAKE'
