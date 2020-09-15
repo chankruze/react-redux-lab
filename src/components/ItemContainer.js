e3705b339ed39931cdbc6482b03aaf80a03997e3
chankruze <chankruze@gmail.com>
2020-09-15 22:16:19 +0530

    Demo of ownProps with mapDispatchToProps
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -7,11 +7,13 @@ Copyright (c) Geekofia 2020 and beyond
 
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIcecream } from '../redux'
 
 function ItemContainer(props) {
     return (
         <div>
             <h2>Num of {props.item}s - {props.count}</h2>
+            <button onClick={props.buyItem}>Buy An Item</button>
         </div>
     )
 }
@@ -30,4 +32,18 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const dispatchFunction = ownProps.cake
+        ? () => dispatch(buyCake())
+        : () => dispatch(buyIcecream())
+
+    return {
+        buyItem: dispatchFunction
+    }
+}
+
+export default connect(
+    // can be null to not subscribe changes
+    mapStateToProps,
+    mapDispatchToProps
+)(ItemContainer)
